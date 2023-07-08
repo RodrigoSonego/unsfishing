e021772bf2f61ee6700ee77e7b8af0c18e91406d
Rodrigo Sonego <rodrigoasonego@gmail.com>
2023-07-08 18:12:16 -0300

    feature: sequence minigame

---
+++ Assets/Minigames/Minigame.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Minigame : MonoBehaviour
+{
+	[SerializeField] protected float maxTime;
+	[SerializeField] private TextMeshProUGUI timerText;
+
+	protected float timeRemaining;
+
+	protected event Action OnTimeRunOut;
+
+	protected virtual void Start()
+	{
+		StartTimer();
+
+		StartCoroutine(UpdateTimer());
+	}
+
+	protected virtual void Update()
+	{
+		//UpdateTimer();
+	}
+
+	private void StartTimer()
+	{
+		timeRemaining = maxTime;
+	}
+
+	private IEnumerator UpdateTimer()
+	{
+		if(timerText == null) { yield break; }
+
+		while (timeRemaining >= 0)
+		{
+			timeRemaining -= Time.deltaTime;
+			timerText.text = "00:" + timeRemaining.ToString("00");
+
+			yield return null;
+		}
+
+		if (OnTimeRunOut != null) { OnTimeRunOut(); }
+	}
+}
---
+++ Assets/Minigames/Sequence/SequenceMinigame.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SequenceMinigame : Minigame
+{
+	[SerializeField] private int sequenceLength;
+	[SerializeField] private List<TextMeshProUGUI> keyLabels;
+
+	[SerializeField] private KeyCode[] KEYS = { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.Q, KeyCode.E };
+
+	[SerializeField] private Queue<KeyCode> sequenceQueue = new Queue<KeyCode>();
+
+	protected override void Start()
+	{
+		if(sequenceLength > keyLabels.Count)
+		{
+			Debug.Log("SEQUENCIA MAIOR QUE NUMERO DE LABELS!!");
+			return;
+		}
+
+		HideKeys();
+		GenerateSequence();
+		
+		ShowSequence();
+
+		base.Start();
+
+		OnTimeRunOut += Defeat;
+    }
+
+	protected override void Update()
+	{
+		if (timeRemaining <= 0)
+		{
+			return;
+		}
+
+		ProcessInput();
+
+		base.Update();
+	}
+
+	private void ProcessInput()
+	{
+		if (sequenceQueue.Count == 0) { return; }
+		if (Input.anyKeyDown == false) { return; }
+		if (Input.GetKeyDown(KeyCode.Mouse0)) { return; }
+
+		int labelIndex = sequenceLength - sequenceQueue.Count;
+
+		if (Input.GetKeyDown(sequenceQueue.Peek()))
+		{
+			keyLabels[labelIndex].color = Color.green;
+
+			sequenceQueue.Dequeue();
+
+			if (sequenceQueue.Count == 0)
+			{
+				OnCompleteSequence();
+			}
+
+			return;
+		}
+
+		keyLabels[labelIndex].color = Color.red;
+	}
+
+	private void OnCompleteSequence()
+	{
+		print("completou a sequencia de boa");
+		//do stuff
+	}
+
+	private void GenerateSequence()
+	{
+		sequenceQueue.Clear();
+
+		for (int i = 0; i < sequenceLength; i++) 
+		{
+			KeyCode randomKey = GetRandomKey();
+
+			// Try not letting key be equal to previous
+			if(i > 0 && randomKey == sequenceQueue.ToArray()[i-1])
+			{
+				randomKey = GetRandomKey();
+			}
+
+			sequenceQueue.Enqueue(randomKey);
+		}
+	}
+
+	private KeyCode GetRandomKey()
+	{
+		return KEYS[Random.Range(0, KEYS.Length)];
+	}
+
+	private void HideKeys()
+	{
+        foreach (var key in keyLabels)
+        {
+			// gore
+            key.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+	private void ShowSequence()
+	{
+		KeyCode[] keyArray = sequenceQueue.ToArray();
+
+		for (int i = 0; i < sequenceLength; i++)
+		{
+			keyLabels[i].transform.parent.gameObject.SetActive(true);
+			keyLabels[i].text = keyArray[i].ToString();
+		}
+    }
+
+	private void Defeat()
+	{
+		print("cabou o tempo da sequencia");
+		//do damage
+	}
+}
