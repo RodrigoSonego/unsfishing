dc24f4d69ae8b85c0ee3161668d8adb729ad47f9
Renan Saldanha <renansaldanha760@gmail.com>
2023-07-09 02:50:53 -0300

    Warida

--- Assets/Script/PeixeController.cs
+++ Assets/Script/PeixeController.cs
@@ -2,54 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class PeixeController : MonoBehaviour
 
 
 {
     bool FacingRight = true;
-    private Rigidbody2D RigidDirection;
+    private Rigidbody2D rb;
+
     public Animator anim;
+
+    public int teste;
+
+    public float rotationForce;
+
     public float Velocidade;
     // Start is called before the first frame update
     void Start()
     {
-        RigidDirection = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+
+        //Movimento Basicão
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector2 movementDirection = new Vector2(horizontal, vertical).normalized * Velocidade;
-        RigidDirection.velocity = movementDirection;
+        Vector3 movementDirection = new Vector3(horizontal, vertical).normalized * Velocidade;
+        rb.velocity = movementDirection;
 
 
-      if (horizontal < 0 && FacingRight)
-      {
-          Flip();
-  
-       }
-
-       else if(horizontal > 0 && !FacingRight)
+      //Animação
+      if(horizontal != 0)
         {
-           Flip();
+            anim.SetBool("Swimming", true);
+            anim.SetBool("isIdle", false);
         }
-        
-       if(movementDirection != Vector2.zero)
+        else
         {
-            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
-    
+            anim.SetBool("isIdle", true);
+            anim.SetBool("Swimming", false);
+        }
+
+      //Flip Basicão
+        if (horizontal < 0 && FacingRight)
+        {
+            Flip();
+
+
+        }
+
+        else if (horizontal > 0 && !FacingRight)
+        {
+            Flip();
+
         }
        
+
+       //Agora uma tentativa de inclinar o peixe
+       if(Input.GetKey(KeyCode.RightArrow))
+        {
+            //Esse aqui ele vai gradualmente tombando
+            // transform.Rotate(Vector3.forward * rotationForce * Time.deltaTime);
+
+            //Esse não
+            Vector3 eulerRotation = transform.rotation.eulerAngles;
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.z = Mathf.Clamp(rotation.z, -20, 0);
+            transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, rotationForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {   
+            Vector3 eulerRotation = transform.rotation.eulerAngles;
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.z = Mathf.Clamp(rotation.z, -20, 0);
+            transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, rotationForce);
+
+            //transform.Rotate(Vector3.forward * rotationForce * Time.deltaTime);
+        }
+
+        if (horizontal == 0)
+        {
+            Vector3 eulerRotation = transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, 0);
+        }
+
+       
+       // Vector3 rotation = transform.rotation.eulerAngles;
+       // rotation.z = Mathf.Clamp(rotation.z, -20, 0);
+       // transform.rotation = Quaternion.Euler(rotation);
     }
 
     void Flip()
    {
+       
+         //transform.Rotate(Vector3.forward * rotationForce * Time.deltaTime);
         FacingRight = !FacingRight;
-        transform.Rotate(0f, 180f, 10f);
+        transform.Rotate(0f, 180f, 0f);
+      
    }
 }
 
